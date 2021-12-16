aa6ce7545172dbd196fb9dece43a3f1f30163b84
mptak12 <56129259+mptak12@users.noreply.github.com>
2021-12-16 23:11:42 +0100

    robot śmiga

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 //korzystamy z kodow z labow TMP2
+
 #include "MKL05Z4.h"
 #include "frdm_bsp.h"
-#include "TPM.h"
-#include "uart0.h"
+#include "motors.h"
+#include "uart0.h" ////przerobic pozniej
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,90 +12,33 @@
 #define MOD_100Hz	6554		// MOD dla czestotliwosci 100Hz
 #define ZEGAR 655360
 
-//#define DELAY(x)   for(uint32_t i=0;i<(x*10000);i++)__NOP();
-
-uint8_t rx_buf_pos=0;
-//char temp,buf;
-uint8_t temp;
-uint8_t rx_buf;
-uint8_t rx_FULL=0;
-uint8_t too_long=0;
-/*
-void UART0_IRQHandler()
-{
-	if(UART0->S1 & UART0_S1_RDRF_MASK)
-	{
-		temp=UART0->D;	// Odczyt wartosci z bufora odbiornika i skasowanie flagi RDRF
-		if(!rx_FULL)
-		{
-			if(temp!=CR)	//to mój pimpeczek napisal spytac sie go <3
-			{	//LAB7 zadanie 2 spoko to tlumaczy
-					rx_buf= temp;	// Zapisz liczbe podana z arduino
-					rx_FULL =1;		//ta zmienna sprawi, ze w petli glownej zacznie sie akcja
-			}
-		}
-	NVIC_EnableIRQ(UART0_IRQn);
-	}
-}
-*/
-
 int main(){
-	float freq;
 	uint16_t	mod_curr=MOD_100Hz;	// Poczatkowy MOD dla f=100Hz
-	uint8_t k_curr=200;					// Poczatkowy wspólczynnik wypelnienia ustawiony zeby stal
-	uint16_t	ampl;
-	//uint8_t choice = 0;
+	//uint8_t k_curr=200;					// Poczatkowy wspólczynnik wypelnienia ustawiony zeby stal
+	//uint16_t	ampl;
 	
-	PWM_Init();				// Inicjalizacja licznika TPM0 (PWM „Low-true pulses”)
+	Motor_Init();
 	
 	TPM0->MOD = mod_curr;		// Poczatkowa wartosc MOD dla f=10Hz
-	ampl=((int)mod_curr*k_curr)/100;	// Wspólczynnik wypelnienia k przeliczony dla aktualnego MOD - realna warotsc CnV
-	
-	//TPM0->CONTROLS[0].CnV = ampl; 	// Sygnal o nowym k podany na PTB11
-	//TPM0->CONTROLS[5].CnV = ampl; 	// PTA5
-	
-	freq=(float)ZEGAR/(float)mod_curr;	// Wyliczenie czestotliwosci na podstawie wartosci zegara i rejestru MOD
-	
-	while(1)
-	{
-		
-		//w=TSI_ReadSlider();
-		/*
-		switch(choice){
-			case 1: //robot do przodu i do tylu
-			k_curr=40;
-			ampl=((int)mod_curr*k_curr)/100;
-			TPM0->CONTROLS[3].CnV = ampl;	// Nowa wartosc kreujaca wspólczynnik wypelnienia PWM
-			TPM0->CONTROLS[2].CnV = ampl;
-			//_sleep_(10);
+	//ampl=((int)mod_curr*k_curr)/100;	// Wspólczynnik wypelnienia k przeliczony dla aktualnego MOD - realna warotsc CnV
+
+	while(1){
 			
-			//do tylu pin 7 i 8 dac na 1
-			TPM0->CONTROLS[3].CnV = ampl;	// Nowa wartosc kreujaca wspólczynnik wypelnienia PWM
-			TPM0->CONTROLS[2].CnV = ampl;
-			//sleep(10);
-			TPM0->CONTROLS[3].CnV = 0;	// Nowa wartosc kreujaca wspólczynnik wypelnienia PWM
-			TPM0->CONTROLS[2].CnV = 0;
-			//sleep(10);
-			DELAY(1000) //sproboj
-			__nop(1000); //spruboj
-			
-			break;
-		}
-		*/
-			k_curr=40;
-			ampl=((int)mod_curr*k_curr)/100;
-			
-			PTB->PDOR |=(0<<7)|(1<<10); 		//tutaj ustawiamy kierunek przod/tyl
-			TPM0->CONTROLS[0].CnV = ampl;	// Nowa wartosc kreujaca wspólczynnik wypelnienia PWM		
-			TPM0->CONTROLS[5].CnV = ampl;
+			set_motor_L(80);		//0-10 stoi raczej 100 - max
+			set_motor_R(80);
 			DELAY(500)
 			
-			k_curr=200;
-			ampl=((int)mod_curr*k_curr)/100;
-			TPM0->CONTROLS[0].CnV = ampl;	// Nowa wartosc kreujaca wspólczynnik wypelnienia PWM		
-			TPM0->CONTROLS[5].CnV = ampl;
-			DELAY(1000)
-			
+			set_motor_L(0);
+			set_motor_R(0);
+			DELAY(500)
+		
+			set_motor_L(-50);
+			set_motor_R(50);
+			DELAY(500)
+		
+			set_motor_L(50);
+			set_motor_R(-50);
+			DELAY(1500)
 	}
-	
+
 }
