f474a885894146ce5835cdd58f506f4273cf869b
mptak12 <56129259+mptak12@users.noreply.github.com>
2021-12-17 11:13:35 +0100

    libraries

---
+++ led.c
@@ -0,0 +1,17 @@
+#include "led.h"
+
+void Zumo_Led_Init(void)
+{	//zeby nie dublowac instrukcji to pominalem te,
+	//ktore sa w motors init
+	//SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK; 
+	PORTB->PCR[0] |= PORT_PCR_MUX(1); //port diody ALT1
+	PTB->PDDR |= (1<<0); //ptb0 rola:wyjscie
+	PTB->PDOR |= (1<<0); //zgas diode
+}
+
+
+void LED(int a)
+{
+	if(a==0) PTB->PDOR |= (1<<0); //gasi //chyba jednak to na odwrot jest 
+		else	PTB->PDOR &= ~(1<<0); //swieci
+}
---
+++ led.h
@@ -0,0 +1,4 @@
+#include "MKL05Z4.h"
+
+void Zumo_Led_Init(void);
+void LED(int);
